91ea6c0a4333993fef9f3d76f50bb4bbdede0096
Aron Puddy <noreply@aronpuddy.com>
2024-05-25 00:56:43 +0930

    fix read.marvel.com link with updated deeplink and fix loading order

--- read_marvel.js
+++ read_marvel.js
@@ -1,50 +1,64 @@
 const SHARE_LINK_PREFIX = "https://share.marvel.com/sharing/legacy/"
+const DEEPLINK_PREFIX = "marvelunlimited://issue/"
+const INSERT_POINT = "span.allPages"
 
-process();
-
-window.addEventListener('hashchange', process);
-
-function process() {
-    console.log("CHANGING PAGE");
+function updatePage() {
     teardown();
-    setup();
+    insertLink();
 }
 
 function teardown() {
-    var element = document.querySelector("#marvelous-link");
+    const element = document.querySelector("#marvelous-link");
     if (element !== null) {
         element.parentNode.removeChild(element);
     }
 }
 
-function setup() {
-    var loc = window.location.toString();
+function insertLink() {
+    const loc = window.location.toString();
 
-    var digitalIdPos  = loc.lastIndexOf("/");
-    var digitalId = loc.slice(digitalIdPos + 1, loc.length);
-    
-    var dataUrl = SHARE_LINK_PREFIX + digitalId;
-    
-    var xhr = new XMLHttpRequest();
+    const digitalIdPos  = loc.lastIndexOf("/");
+    const digitalId = loc.slice(digitalIdPos + 1, loc.length);
+
+    const dataUrl = SHARE_LINK_PREFIX + digitalId;
+
+    const xhr = new XMLHttpRequest();
     xhr.addEventListener("readystatechange", function () {
-        if(this.readyState === 4) {
-            const regex = /href="(.*?.smart.*?)"/g
-            var match = regex.exec(this.responseText);
-            var newUrl = match[1];
-    
-            var span = document.createElement("span");
+        if(this.readyState == 4) {
+            const regex = /drn=(.*)&amp/gm
+            const match = regex.exec(this.responseText);
+            const data = match[1];
+            const newUrl = DEEPLINK_PREFIX + data;
+
+            const span = document.createElement("span");
             span.id="marvelous-link"
             span.style="float:left; overflow: visible; top: 0px; text-indent: 20%;";
-            var anchor = document.createElement("a");
+            const anchor = document.createElement("a");
             anchor.setAttribute("href", newUrl);
             anchor.innerText = "Open in app";
             anchor.style = "color:white";
             span.appendChild(anchor);
-    
-            var header = document.body.querySelector("span.allPages");
+
+            const header = document.body.querySelector(INSERT_POINT);
             header.after(span);
         }
     });
     xhr.open("GET", dataUrl);
     xhr.send();
 }
+
+const observer = new MutationObserver((mutations, observer) => {
+    for (const mutation of mutations) {
+        if (mutation.type === 'childList') {
+            for (const node of mutation.addedNodes) {
+                if (node.nodeType === Node.ELEMENT_NODE && node.querySelector(INSERT_POINT)) {
+                    updatePage();
+                    observer.disconnect();
+                    return;
+                }
+            }
+        }
+    }
+});
+
+observer.observe(document.body, {childList: true, subtree: true});
